0d093a7dc93ced18e13ba901b30bb54d532a2c72
lyman wong <lymanwong@gmail.com>
2016-03-03 15:30:00 -0800

    updated

--- example2/js/controllers/MainController.js
+++ example2/js/controllers/MainController.js
@@ -19,4 +19,41 @@ app.controller('MainController', ['$scope', function($scope) {
     developer: 'Armando P.',
     price: 1.99
   };
+
+  $scope.forecast = {
+    icon: 'img/ava.jpg',
+    title: 'Wedding',
+    developer: 'Joe Mamma',
+    price: 19.99
+  };
+
+  $scope.apps = [
+    {
+    icon: 'img/lyman_headshot.jpg',
+    title: 'MOVE',
+    developer: 'MOVE, Inc.',
+    price: 0.99
+  },
+
+  {
+    icon: 'img/marss_freyja.jpg',
+    title: 'Shutterbugg',
+    developer: 'Chico Dusty',
+    price: 2.99
+  },
+
+  {
+    icon: 'img/marssy_niners.jpg',
+    title: 'Gameboard',
+    developer: 'Armando P.',
+    price: 1.99
+  },
+
+  {
+    icon: 'img/ava.jpg',
+    title: 'Wedding',
+    developer: 'Joe Mamma',
+    price: 19.99
+  }
+  ]
 }]);
